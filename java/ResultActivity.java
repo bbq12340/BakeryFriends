bad4d75896892fc7305497fb876490d851e1cb30
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-12-16 18:59:17 +0900

    Update ResultActivity.java

--- java/ResultActivity.java
+++ java/ResultActivity.java
@@ -1,4 +1,25 @@
-package com.example.baking_friends;
+package com.example.bakingfriends;
 
-public class ResultActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.res.Resources;
+
+import java.util.ArrayList;
+
+public class ResultActivity extends Activity {
+    String bakery;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String choice = intent.getStringExtra("choice"); // "cookie"
+        ArrayList<String> myIngredient = getIntent().getStringArrayListExtra("myIngredient");
+        Object[] myIngredientArray = myIngredient.toArray();
+
+
+    }
 }
