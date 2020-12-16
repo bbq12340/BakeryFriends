1e59bf55ad535a5af05219ae5656eee9ff71b6e8
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-12-16 19:00:59 +0900

    Update MainActivity.java
    인텐트로 넘기는 것 까지 (베이커리 종류와 재료)

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -11,9 +11,11 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStart, btnEnter;
+    Button btnStart;
+    ListView bakeryChoiceList;
     EditText Ingredient;
-    ListView listIng;
+    String [] bakeryChoice;
+    String query, choice;
 
 
 
@@ -22,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Baking Friends");
+        
+        btnStart = (Button) findViewById(R.id.btnStart);
+        Ingredient = (EditText) findViewById(R.id.Ingredient);
+        bakeryChoiceList = (ListView) findViewById(R.id.bakeryChoice);
+
+        bakeryChoiceList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.bakeryChoice,
+                android.R.layout.simple_list_item_single_choice);
+        bakeryChoiceList.setAdapter(adapter);
+        bakeryChoice = getResources().getStringArray(R.array.bakeryChoice);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                query = Ingredient.getText().toString();
+                choice = bakeryChoice[bakeryChoiceList.getCheckedItemPosition()].toString();
+                List<String> myIngredient = new ArrayList<String>(Arrays.asList(query.split(",")));
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("choice", choice);
+                intent.putStringArrayListExtra("myIngredient", (ArrayList<String>) myIngredient);
+                startActivity(intent);
+            }
+        });
     }
 
 
