207a7d6ce459ed27cfb08ec6d41502a9eb440be4
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-12-17 18:09:40 +0900

    Update ResultActivity.java

--- java/ResultActivity.java
+++ java/ResultActivity.java
@@ -3,6 +3,8 @@ package com.example.bakingfriends;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,8 +17,10 @@ public class ResultActivity extends Activity {
     String [] bakeryIngredient; // 선택된 베이커리 재
     int [] countScore; // 재료 일치 점수
     int [] rank; // 베이커리 재료 일치 순위
-    int max, t;
     TextView recommend1, recommend2, recommend3;
+    int first, second, third;
+    int r1, r2, r3;
+    ImageButton recommendImg1, recommendImg2, recommendImg3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +48,63 @@ public class ResultActivity extends Activity {
                     ;
                 }
             }
+            countScore[i] = countScore[i]/myIngredientArray.length;
         }
 
         recommend1 = (TextView) findViewById(R.id.recommend1);
         recommend2 = (TextView) findViewById(R.id.recommend2);
         recommend3 = (TextView) findViewById(R.id.recommend3);
 
-        max=0;
+        recommendImg1 = (ImageButton) findViewById((R.id.recommendImg1));
+        recommendImg2 = (ImageButton) findViewById((R.id.recommendImg2));
+        recommendImg3 = (ImageButton) findViewById((R.id.recommendImg3));
+
+        first=0;
+        second=0;
+        third=0;
         for (int i=0; i<countScore.length; i++) {
-            if (countScore[i]>max) {
-                max = countScore[i];
-                t=i;
+            if (countScore[i]>first) {
+                third = second;
+                second = third;
+                first = countScore[i];
+                r1=i;
+            } else if (countScore[i]>second) {
+                third = second;
+                second = countScore[i];
+                r2=i;
+            } else if (countScore[i]>third) {
+                third=countScore[i];
+                r3=i;
             }
         }
-        recommend1.setText(bakery[t]);
+        recommend1.setText(bakery[r1]);
+        recommend2.setText(bakery[r2]);
+        recommend3.setText(bakery[r3]);
+
+        int r1Id = this.getResources().getIdentifier(bakery[r1], "drawable", this.getPackageName());
+        int r2Id = this.getResources().getIdentifier(bakery[r2], "drawable", this.getPackageName());
+        int r3Id = this.getResources().getIdentifier(bakery[r3], "drawable", this.getPackageName());
+        recommendImg1.setImageResource(r1Id);
+        recommendImg2.setImageResource(r2Id);
+        recommendImg3.setImageResource(r3Id);
+
+        recommendImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        recommendImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        recommendImg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
